ab9ace0cd1f9069409fc658edc54b5eadc897c61
Akhil Ajith Nair <akhilajithnair@gmail.com>
2022-04-26 21:11:38 +0530

    Add page components

--- Abode.Decors.Frontend/src/app/app.module.ts
+++ Abode.Decors.Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    PartnersComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
